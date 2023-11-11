b97d2ae0f902b406e7e7cc1bae54e3dfd9e61a42
Azimute Norte <azimutenortevarginha@gmail.com>
2023-11-11 16:00:56 -0300

    qualquer coisa

---
+++ src/components/SaibaMais/index.tsx
@@ -0,0 +1,86 @@
+import * as S from "./styles"
+import Email from "../../assets/email.png"
+import Insta from "../../assets/insta.png"
+
+export function saiba_mais() {
+  return (
+    <S.SaibaM>
+      <ul>
+        <li id="titulo">
+          <a href="#itens">SAIBA MAIS</a>
+        </li>
+        <li>
+          <nav id="itens">
+            <S.Quadros>
+                <ul>
+                    <li>
+                      <h2>Fale conosco!</h2>
+                    </li>
+                    <li id="Cont">
+                      <ul id="C">
+                        <li id="Gabi">
+                          <ul>
+                            <li id="nome">
+                              <h3>Coordenadora Gabriela Arantes</h3>
+                            </li>
+                            <li id="email">
+                              <h3>gabrielaarantes@cefetmg.br</h3>
+                            </li>
+                          </ul>
+                        </li>
+                        <li id="Wag">
+                          <ul>
+                            <li id="nome">
+                              <h3>Colaborador Wagner Silva</h3>
+                            </li>
+                            <li id="email">
+                              <h3>wagnersilva@cefetmg.br</h3>
+                            </li>
+                          </ul>
+                        </li>
+                        <li id="Az">
+                          <ul>
+                            <li id="img">
+                            <img src={Email} alt="E-mail icon" />
+                            </li>
+                            <li id="email">
+                              <h3>azimutenortevarginha@gmail.com</h3>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+            </S.Quadros>
+            <S.Espaco/>
+            <S.Quadros>
+                <ul>
+                    <li>
+                      <h2>Nos acompanhe!</h2>
+                    </li>
+                    <li id="Cont">
+                      <ul>
+                        <li id="Texto">
+                          <p>Clique <span><a href="">aqui</a></span> para<br/>receber nosso<br/>boletim mensal!</p>
+                        </li>
+                        <li id="Az">
+                          <ul id="Insta">
+                            <li id="img">
+                            <img src={Insta} alt="Instagram icon" />
+                            </li>
+                            <li>
+                              <h3>@programaazimute</h3>
+                            </li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li>
+                  </ul>
+            </S.Quadros>
+          </nav>
+        </li>
+      </ul>
+    </S.SaibaM>
+  )
+}
+
---
+++ src/components/SaibaMais/styles.ts
@@ -0,0 +1,130 @@
+import Orientista from "../../assets/Orientista.png"
+import styled from "styled-components"
+import { colors } from "../../styles/GlobalStyle"
+
+export const SaibaM = styled.nav`
+  /* Cabeçalho */
+    display: flex;
+    flex-direction: row;
+    background-image: linear-gradient(${colors.laranjaC}, ${colors.laranjaE});
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    ul {
+        list-style-type: none;
+        width: 100%;
+        li#titulo {
+            border-bottom-style: solid;
+            border-bottom-color: ${colors.azul};
+            border-bottom-width: 0.3rem;
+            align-items: center;
+            height: 4.4rem;
+            width: 100%;
+            a {
+                text-decoration: none;
+                color: ${colors.azul};
+                font-family: "Passion One";
+                font-size: 4rem;
+                width: 100%;
+                padding-left: 2rem;
+            }
+        }
+        li nav#itens{
+            background-image: url("/Orientista.png");
+            display: flex;
+            width: 100%;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            padding: 4rem;
+        }
+    }
+`
+export const Quadros = styled.nav`
+    padding: 1rem;
+    ul {
+        list-style-type: none;
+        li#Cont {
+            padding: 3rem;
+            border: 0.3rem solid ${colors.branco};
+            border-radius: 1rem;
+            ul#C {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                ul {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                }
+                li#Az {
+                    padding: 1rem;
+                    ul {
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: center;
+                        align-items: center;
+                        li#email h3{
+                            padding: 1rem;
+                            font-size: 2rem;
+                        }
+                    }
+                }
+            }
+            li#Texto p {
+                font-family: "Passion One";
+                font-size: 2.5rem;
+                color: ${colors.branco};
+                padding: 1rem;
+                text-align: center;
+                span {
+                    padding: 1rem;
+                    padding-top: 2rem;
+                    background: url("/aqui.png");
+                    background-size: 6rem;
+                    background-repeat: no-repeat;
+                    background-position: top;
+                    a {
+                        font-family: "Passion One";
+                        font-size: 2.5rem;
+                        color: ${colors.branco};
+                    }
+                }
+            }
+        }
+        h2 {
+            font-family: "Passion One";
+            font-size: 2.5rem;
+            color: ${colors.branco};
+        }
+        li#nome h3{
+            color: ${colors.azul};
+            font-family: "Passion One";
+            font-size: 2rem;
+        }
+        li#email h3{
+            color: ${colors.branco};
+            font-family: "Passion One";
+            font-size: 1.5rem;
+        }
+        ul#Insta {
+            list-style-type: none;
+            display: flex;
+            flex-direction: row;
+            h3 {
+                color: ${colors.branco};
+                font-family: "Passion One";
+                font-size: 2rem;
+                padding: 1rem;
+            }
+        }
+
+
+    }
+`
+
+export const Espaco = styled.nav`
+    padding: 7rem;
+`
